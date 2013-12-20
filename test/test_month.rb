57f456c4273b746b59962fde12cf9cee29db7975
Lori McCurry <lori.mccurry@gmail.com>
2013-12-20 15:13:29 -0600

    fix zeller unit tests

--- test/test_month.rb
+++ test/test_month.rb
@@ -2,61 +2,57 @@ require 'month'
 require 'minitest/autorun'
 
 class TestMonth < MiniTest::Unit::TestCase
-  def test_month_has_zeller_method
-    # Month.test_method("arg")
-  end
 
   def test_01_test_zellers_base_case_01_2012
-    month_year_request = '01 2012'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Sunday', month_first_day)
+    new_month = Month.new
+    assert_equal(1, new_month.zeller(1, 2012))
   end
 
   def test_02_test_zellers_leap_year_02_2012
-    month_year_request = '02 2012'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Wednesday', month_first_day)
+    new_month = Month.new
+    assert_equal(4, new_month.zeller(2, 2012))
   end
 
   def test_03_test_zellers_6_week_month_09_2012
-    month_year_request = '09 2012'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Saturday', month_first_day)
+    new_month = Month.new
+    assert_equal(0, new_month.zeller(9, 2012))
   end
 
   def test_04_test_zellers_4_week_month_02_2015
-    month_year_request = '02 2015'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Sunday', month_first_day)
+    new_month = Month.new
+    assert_equal(1, new_month.zeller(2, 2015))
   end
 
   def test_05_test_zellers_non_leap_century_02_1900
-    month_year_request = '02 1900'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Thursday', month_first_day)
+    new_month = Month.new
+    assert_equal(5, new_month.zeller(2, 1900))
   end
 
   def test_06_test_zellers_leap_century_02_2000
-    month_year_request = '02 2000'
-    month_first_day = Month.zeller(month_year_request)
-    assert_equal('Tuesday', month_first_day)
+    new_month = Month.new
+    assert_equal(3, new_month.zeller(2, 2000))
   end
 
   def test_07_test_zellers_outside_of_scope_05_3005
-    month_year_request = '05 3005'
-    month_first_day = Month.zeller(month_year_request)
+    new_month = Month.new
     assert_raises ArgumentError do
-      #########what goes here???#############
+      new_month.zeller(5, 3005)
     end
   end
 
-  def test_08_test_zellers_no_arguments
-    month_year_request = ''
-    month_first_day = Month.zeller(month_year_request)
+  def test_08_test_zellers_outside_of_scope_05_1799
+    new_month = Month.new
+    #b/c passing in via argv
     assert_raises ArgumentError do
-      #########what goes here???#############
+      new_month.zeller(5, 1799)
     end
   end
 
+  def test_09_test_zellers_no_arguments
+    new_month = Month.new
+    assert_raises ArgumentError do
+      new_month.zeller('')
+    end
+  end
 
 end
