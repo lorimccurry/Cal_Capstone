43ba56e45cc30f58802769db80290eaae54dc11e
Lori McCurry <lori.mccurry@gmail.com>
2013-12-20 15:15:05 -0600

    initial commit cal.rb file

---
+++ cal.rb
@@ -0,0 +1,9 @@
+require_relative 'lib/year'
+require_relative 'lib/month'
+
+#need code to say if there's only 1 arg (argv0 is nil, that it's a year; w/ 2, see below; with
+
+month = ARGV[0]
+year = ARGV[1]
+
+puts `cal #{month} #{year}`
