995d32c0ee6e47e5640072f86859959a18865e12
Lori McCurry <lori.mccurry@gmail.com>
2013-12-21 15:31:32 -0600

    refactor and fix month_name

--- lib/month.rb
+++ lib/month.rb
@@ -2,10 +2,16 @@ require_relative 'zeller'
 
 class Month
 
-  def month_name(month_num)
+  def initialize (month, year)
+    @month = month
+    @year = year
+
+  end
+
+  def month_name
     month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    name = month_names[month_num -= 1]
-    formatted_name = name.center(20)
+    name = month_names[@month - 1]
+    first_row = "#{name} #{@year}".center(20)
   end
 
 
--- test/test_month.rb
+++ test/test_month.rb
@@ -5,24 +5,24 @@ require 'minitest/autorun'
 class TestMonth < MiniTest::Unit::TestCase
 
   def test_01_test_month_name_conversion_January
-    new_month = Month.new
-    assert_equal("      January       ", new_month.month_name(1))
+    new_month = Month.new(1, 2012)
+    assert_equal("    January 2012    ", new_month.month_name)
   end
 
   def test_02_test_month_name_conversion_May
-    new_month = Month.new
-    assert_equal("        May         ", new_month.month_name(5))
+    new_month = Month.new(5, 2012)
+    assert_equal("      May 2012      ", new_month.month_name)
   end
 
   def test_03_test_month_name_row_length
-    new_month = Month.new
-    row = new_month.month_name(1)
+    new_month = Month.new(1, 2012)
+    row = new_month.month_name
     assert_equal(20, row.length)
   end
 
   def test_04_test_month_name_row_length
-    new_month = Month.new
-    row = new_month.month_name(5)
+    new_month = Month.new(5, 2012)
+    row = new_month.month_name
     assert_equal(20, row.length)
   end
 
