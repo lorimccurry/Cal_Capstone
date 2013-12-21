5ef3d297e615122816c9f41c89d8183b74510425
Lori McCurry <lori.mccurry@gmail.com>
2013-12-21 11:45:43 -0600

    reinstate zeller class

--- cal.rb
+++ cal.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/year'
 require_relative 'lib/month'
+require_relative 'lib/zeller'
 
 #need code to say if there's only 1 arg (argv0 is nil, that it's a year; w/ 2, see below; with
 
