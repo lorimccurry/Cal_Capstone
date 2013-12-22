e5cb59ba847bb558c0532a7b8cbc097d2e3f86f9
Lori McCurry <lori.mccurry@gmail.com>
2013-12-22 16:07:43 -0600

    zeller commented out

--- lib/zeller.rb
+++ lib/zeller.rb
@@ -1,22 +1,22 @@
 class Zeller
 
-  def zeller(*inputs)
-    month = inputs[0].to_i
-    year = inputs[1].to_i
+  # def zeller(*inputs)
+  #   month = inputs[0].to_i
+  #   year = inputs[1].to_i
 
-    if year < 1800 || year > 3000
-      raise ArgumentError, 'Year out of range'
-    end
+  #   if year < 1800 || year > 3000
+  #     raise ArgumentError, 'Year out of range'
+  #   end
 
-    if inputs == nil
-        raise ArgumentError, 'No arguments given'
-      end
+  #   if inputs == nil
+  #       raise ArgumentError, 'No arguments given'
+  #     end
 
-    if inputs[0] == 1 or inputs[0] == 2
-      year -= 1
-      month += 12
-    end
+  #   if inputs[0] == 1 or inputs[0] == 2
+  #     year -= 1
+  #     month += 12
+  #   end
 
-    day_one = (1 + ((month + 1)*26)/10 + year + (year/4) + 6*(year/100) + (year/400)) % 7
-  end
+  #   day_one = (1 + ((month + 1)*26)/10 + year + (year/4) + 6*(year/100) + (year/400)) % 7
+  # end
 end
