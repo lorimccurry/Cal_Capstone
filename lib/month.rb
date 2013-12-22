fcd65e04ed332345682334acf8be39cd2525f72c
Lori McCurry <lori.mccurry@gmail.com>
2013-12-21 23:45:51 -0600

    days/month in process

--- lib/month.rb
+++ lib/month.rb
@@ -15,5 +15,40 @@ class Month
     header = first_row + second_row
   end
 
+  def month_days
+
+
+
+    thirty_one = [1, 3, 5, 7, 8, 10, 12]
+    thirty = [4, 6, 9, 11]
+
+    if thirty_one.include? @month
+      num_days = 31
+      # puts num_days
+    elsif thirty.include? @month
+      num_days = 30
+      # puts num_days
+    # elsif
+    #   leap_year == true ? num_days = 29 : num_days = 28
+      # puts num_days
+    end
+
+    if @month == 2 && @year % 400 == 0
+      leap_year = true
+      # print leap_year
+    elsif @month == 2 && @year % 100 == 0
+      leap_year = false
+      # print leap_year
+    elsif @month == 2 && @year % 4 == 0
+      leap_year = true
+      # print leap_year
+    else
+      leap_year = false
+      # print leap_year
+    end
+    leap_year == true ? num_days = 29 : num_days = 28
+
+  end
+
 
 end
--- test/test_month.rb
+++ test/test_month.rb
@@ -22,4 +22,34 @@ EOS
     assert_equal(expected.chomp, new_month.head_rows)
   end
 
+  def test_03_test_days_in_month_1_2012
+    new_month = Month.new(1, 2012)
+    assert_equal(31, new_month.month_days)
+  end
+
+  def test_04_test_days_in_month_2_2012
+    new_month = Month.new(2, 2012)
+    assert_equal(29, new_month.month_days)
+  end
+
+  def test_05_test_days_in_month_9_2012
+    new_month = Month.new(9, 2012)
+    assert_equal(30, new_month.month_days)
+  end
+
+  def test_06_test_days_in_month_2_2015
+    new_month = Month.new(2, 2015)
+    assert_equal(28, new_month.month_days)
+  end
+
+  def test_07_test_days_in_month_2_1900
+    new_month = Month.new(2, 1900)
+    assert_equal(28, new_month.month_days)
+  end
+
+  def test_08_test_days_in_month_2_2000
+    new_month = Month.new(2, 2000)
+    assert_equal(29, new_month.month_days)
+  end
+
 end
