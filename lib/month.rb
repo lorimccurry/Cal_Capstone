7c6ea965e1a3347661764d8e672ddea7e7acb5f0
Lori McCurry <lori.mccurry@gmail.com>
2013-12-22 16:08:48 -0600

    fixing number of days method

--- lib/month.rb
+++ lib/month.rb
@@ -1,10 +1,21 @@
-require_relative 'zeller'
 
 class Month
 
   def initialize (month, year)
     @month = month
     @year = year
+  end
+
+  def zeller
+    month = @month
+    year = @year
+
+    if month == 1 or month == 2
+      year -= 1
+      month += 12
+    end
+
+    day_one = (1 + ((month + 1)*26)/10 + year + (year/4) + 6*(year/100) + (year/400)) % 7
   end
 
   def head_rows
@@ -16,38 +27,34 @@ class Month
   end
 
   def month_days
-
-
+    if @month == 2
+      if @year % 400 == 0
+        leap_year = true
+      elsif @year % 100 == 0
+        leap_year = false
+      elsif @year % 4 == 0
+        leap_year = true
+      end
+    else
+      leap_year = false
+    end
 
     thirty_one = [1, 3, 5, 7, 8, 10, 12]
     thirty = [4, 6, 9, 11]
 
-    if thirty_one.include? @month
-      num_days = 31
-      # puts num_days
-    elsif thirty.include? @month
-      num_days = 30
-      # puts num_days
-    # elsif
-    #   leap_year == true ? num_days = 29 : num_days = 28
-      # puts num_days
-    end
-
-    if @month == 2 && @year % 400 == 0
-      leap_year = true
-      # print leap_year
-    elsif @month == 2 && @year % 100 == 0
-      leap_year = false
-      # print leap_year
-    elsif @month == 2 && @year % 4 == 0
-      leap_year = true
-      # print leap_year
+    if @month == 2
+      if leap_year == true
+        num_days = 29
+      else
+        num_days = 28
+      end
     else
-      leap_year = false
-      # print leap_year
+      if thirty_one.include? @month
+        num_days = 31
+      elsif thirty.include? @month
+        num_days = 30
+      end
     end
-    leap_year == true ? num_days = 29 : num_days = 28
-
   end
 
 
