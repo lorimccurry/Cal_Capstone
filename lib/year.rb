8050e8fb53cd1fde8731cd850828488e795d1f5d
Lori McCurry <lori.mccurry@gmail.com>
2013-12-28 16:48:46 -0600

    year status to date

--- lib/year.rb
+++ lib/year.rb
@@ -1,8 +1,37 @@
 require 'month'
-require 'zeller'
 
 class Year
 
+  def initialize(year)
+    @year = year
+  end
+
+  def year_head
+    first_row = "#{@year}".center(60).rstrip
+    #why is this not 64 centered?
+  end
+
+  def year_body
+    cal_month_array = month_array
+    month_row = []
+    while cal_month_array.length >= 3
+      row = cal_month_array.shift(3)
+      row_string ="#{row[0]} #{row[1]} #{row[2]}"
+      # month_row << row_string.join(" ")
+    end
+    # month_row.join("\n")
+  end
+
+  def month_array
+    month_array = (1..12).to_a
+    cal_month_array = []
+    formatted_year = month_array.each do |month_num|
+      new_month = Month.new(month_num, @year)
+      formatted_month = new_month.month_all(true)
+      cal_month_array.push(formatted_month)
+    end
+    cal_month_array
+  end
 
 
 
--- test/test_year.rb
+++ test/test_year.rb
@@ -1,6 +1,6 @@
 require 'year'
 require 'month'
-require 'zeller'
+
 require 'minitest/autorun'
 
 
@@ -9,6 +9,71 @@ class TestYear < MiniTest::Unit::TestCase
   #   Year.test_method("arg")
   # end
 
+  def test_01_year_number_header
+    new_year = Year.new(2012)
+    expected = <<EOS
+                            2012
+EOS
+    assert_equal(expected.chomp, new_year.year_head)
+  end
 
+  def test_02_year_number_header
+    new_year = Year.new(1800)
+    expected = <<EOS
+                            1800
+EOS
+    assert_equal(expected.chomp, new_year.year_head)
+  end
+
+#   def test_03_joining_month_names_row
+#     new_year = Year.new(2012)
+#     expected = <<EOS
+#       January               February               March
+# EOS
+#   assert_equal(expected.chomp, new_year.year_body)
+#   end
+
+
+  def test_21_year_printing
+    new_year = Year.new(2012)
+    expected = <<EOS
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4               1  2  3
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   4  5  6  7  8  9 10
+15 16 17 18 19 20 21  12 13 14 15 16 17 18  11 12 13 14 15 16 17
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  18 19 20 21 22 23 24
+29 30 31              26 27 28 29           25 26 27 28 29 30 31
+
+
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7         1  2  3  4  5                  1  2
+ 8  9 10 11 12 13 14   6  7  8  9 10 11 12   3  4  5  6  7  8  9
+15 16 17 18 19 20 21  13 14 15 16 17 18 19  10 11 12 13 14 15 16
+22 23 24 25 26 27 28  20 21 22 23 24 25 26  17 18 19 20 21 22 23
+29 30                 27 28 29 30 31        24 25 26 27 28 29 30
+
+
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4                     1
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   2  3  4  5  6  7  8
+15 16 17 18 19 20 21  12 13 14 15 16 17 18   9 10 11 12 13 14 15
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  16 17 18 19 20 21 22
+29 30 31              26 27 28 29 30 31     23 24 25 26 27 28 29
+                                            30
+
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6               1  2  3                     1
+ 7  8  9 10 11 12 13   4  5  6  7  8  9 10   2  3  4  5  6  7  8
+14 15 16 17 18 19 20  11 12 13 14 15 16 17   9 10 11 12 13 14 15
+21 22 23 24 25 26 27  18 19 20 21 22 23 24  16 17 18 19 20 21 22
+28 29 30 31           25 26 27 28 29 30     23 24 25 26 27 28 29
+                                            30 31
+EOS
+    assert_equal(expected.chomp, new_year.year_body)
+  end
 
 end
