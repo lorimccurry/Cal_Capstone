a6b15be4a914c1d3a086f23b663bac306a4388b7
Lori McCurry <lori.mccurry@gmail.com>
2013-12-29 16:35:22 -0600

    minor working changes cal.rb

--- cal.rb
+++ cal.rb
@@ -1,10 +1,11 @@
 require_relative 'lib/year'
 require_relative 'lib/month'
-require_relative 'lib/zeller'
+
 
 #need code to say if there's only 1 arg (argv0 is nil, that it's a year; w/ 2, see below; with
 
 month = ARGV[0]
 year = ARGV[1]
 
+puts month
 puts `cal #{month} #{year}`
