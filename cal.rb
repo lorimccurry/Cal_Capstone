96d216e3c77bb121c2c3205705e2b17c138084c1
Lori McCurry <lori.mccurry@gmail.com>
2014-01-01 17:23:50 -0600

    cal year and month test and code setup

--- cal.rb
+++ cal.rb
@@ -4,8 +4,36 @@ require_relative 'lib/month'
 
 #need code to say if there's only 1 arg (argv0 is nil, that it's a year; w/ 2, see below; with
 
-month = ARGV[0]
-year = ARGV[1]
+if ARGV.length > 2
+  raise ArgumentError, 'Too many arguments'
+elsif ARGV.empty?
+  raise ArgumentError, 'No arguments given'
+elsif ARGV.length == 2
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+elsif ARGV.length == 1
+  year = ARGV[0].to_i
+end
 
-puts month
-puts `cal #{month} #{year}`
+if month < 1 or month > 12 && year < 1800 or year > 3000
+  raise ArgumentError, 'Not a valid month (1..12) and year range (1800..3000)'
+elsif month < 1 or month > 12
+  raise ArgumentError, 'Not a valid month number (1..12)'
+elsif year < 1800 or year > 3000
+  raise ArgumentError, 'Year out of range (1800..3000)'
+end
+
+if ARGV.length == 2
+  new_month = Month.new(month, year)
+  month_array = new_month.week_arrays(false)
+  print month_array
+elsif ARGV.length == 1
+  new_year = Year.new(year)
+  header = new_year.year_head.center(64)
+  year_array = new_year.year_formatted_array
+  print year_output = header + year_array
+else
+  raise ArgumentError, "Houston, we have a problem"
+end
+
+# puts `cal #{month} #{year}`
--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -1,5 +1,5 @@
-# require 'minitest/autorun'
-# require 'cal'
+require 'minitest/autorun'
+# require_relative '../cal.rb'
 require_relative 'helper'
 
 class TestCalIntegration < MiniTest::Unit::TestCase
@@ -7,20 +7,136 @@ class TestCalIntegration < MiniTest::Unit::TestCase
 #test here for cal.rb stuff; test argv values
 #also if year is between 1800 and 3000
 
-  def test_01_cal_inputs
+  def test_01_ARGV_inputs
+    shell_output = `ruby cal.rb 2 2012`
+    assert_equal(["2", "2012"], ARGV)
   end
 
-  def test_000_cal_integration
-#     shell_output = `ruby cal.rb 02 2012` #like the command line
-#     expected_output = <<EOS
-#    February 2012
-# Su Mo Tu We Th Fr Sa
-#           1  2  3  4
-#  5  6  7  8  9 10 11
-# 12 13 14 15 16 17 18
-# 19 20 21 22 23 24 25
-# 26 27 28 29
-# EOS
-#     assert_equal expected_output, shell_output
+  def test_02_month_output_1_2012
+    shell_output = `ruby cal.rb 1 2012`
+    expected_output = <<EOS
+    January 2012
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31#{"            "}
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_03_month_output_2_2012
+    shell_output = `ruby cal.rb 02 2012`
+    expected_output = <<EOS
+   February 2012
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_04_month_output_9_2012
+    shell_output = `ruby cal.rb 9 2012`
+    expected_output = <<EOS
+   September 2012
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30#{"                  "}
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+
+  def test_05_month_output_2_2015
+    shell_output = `ruby cal.rb 02 2015`
+    expected_output = <<EOS
+   February 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_06_month_output_2_1900
+    shell_output = `ruby cal.rb 2 1900`
+    expected_output = <<EOS
+   February 1900
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28#{"         "}
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_07_month_output_2_2000
+    shell_output = `ruby cal.rb 2 2000`
+    expected_output = <<EOS
+   February 2000
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29#{"            "}
+EOS
+    assert_equal expected_output, shell_output
+  end
+
+  def test_08_year_output_2012
+    shell_output = `ruby cal.rb 2012`
+    expected_output = <<EOS
+                            2012
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4               1  2  3
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   4  5  6  7  8  9 10
+15 16 17 18 19 20 21  12 13 14 15 16 17 18  11 12 13 14 15 16 17
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  18 19 20 21 22 23 24
+29 30 31              26 27 28 29           25 26 27 28 29 30 31
+
+
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7         1  2  3  4  5                  1  2
+ 8  9 10 11 12 13 14   6  7  8  9 10 11 12   3  4  5  6  7  8  9
+15 16 17 18 19 20 21  13 14 15 16 17 18 19  10 11 12 13 14 15 16
+22 23 24 25 26 27 28  20 21 22 23 24 25 26  17 18 19 20 21 22 23
+29 30                 27 28 29 30 31        24 25 26 27 28 29 30
+
+
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7            1  2  3  4                     1
+ 8  9 10 11 12 13 14   5  6  7  8  9 10 11   2  3  4  5  6  7  8
+15 16 17 18 19 20 21  12 13 14 15 16 17 18   9 10 11 12 13 14 15
+22 23 24 25 26 27 28  19 20 21 22 23 24 25  16 17 18 19 20 21 22
+29 30 31              26 27 28 29 30 31     23 24 25 26 27 28 29
+                                            30
+
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6               1  2  3                     1
+ 7  8  9 10 11 12 13   4  5  6  7  8  9 10   2  3  4  5  6  7  8
+14 15 16 17 18 19 20  11 12 13 14 15 16 17   9 10 11 12 13 14 15
+21 22 23 24 25 26 27  18 19 20 21 22 23 24  16 17 18 19 20 21 22
+28 29 30 31           25 26 27 28 29 30     23 24 25 26 27 28 29
+                                            30 31
+EOS
+    assert_equal expected_output, shell_output
   end
 end
