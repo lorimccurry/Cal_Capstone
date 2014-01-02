c7feca996de680d9de140f7aeb017fb603ee7de1
Lori McCurry <lori.mccurry@gmail.com>
2014-01-02 14:51:45 -0600

    7 or 8 week month logic

--- lib/year.rb
+++ lib/year.rb
@@ -9,14 +9,13 @@ class Year
   def year_head
     first_row = "#{@year}".center(64)
   end
-#if a month has rows length of 7, then (7-row.length).times do month rows << ["blank row"]
 
   def year_formatted_array
     quarters_array = year_array_raw
     rows = []
     0.upto(3) do |quarter_index|
-      0.upto(6) do |row_index|
-      # 0.upto(7) do |row_index|
+      # 0.upto(6) do |row_index|
+      0.upto(7) do |row_index|
         row = []
         0.upto(2) do |month_index|
           row << quarters_array[quarter_index][month_index][row_index]
@@ -25,21 +24,6 @@ class Year
       end
     end
     rows.unshift(self.year_head)
-
-    #32 rows, each row is a row to print out w/ values from 3 mos
-    # rows.each do |row|
-    #   row.each do |value|
-    #     value.map do |x|
-    #       print !x.nil
-    #     end
-
-      # if row!nil?.length <= 2
-        # print row
-      #   row = "                    "
-      # else
-      #   row = row
-      # end
-    # end
   end
 
   def year_array_raw
@@ -51,28 +35,31 @@ class Year
       cal_month_array.push(month_array)
     end
     quarters = quarter_array(cal_month_array)
-    #   # quarters.each do |month|
-    #   0.upto(3) do |quarter_index|
-    #     0.upto(2) do |month_index|
-    #       quarters[quarter_index].each do |month|
-    #         print quarter_index
-    #       # print s.length
-    #       end
-    #     end
-    #   end
-    # print quarters[2][0].length
-    # print quarters[2][0].length
   end
-
-        #   if row_index = nil
-        #   print row[6]
-        # end
 
   def quarter_array(cal_month_array)
     month_row = []
     while cal_month_array.length >= 3
       quarter = cal_month_array.shift(3)
       month_row << quarter
+    end
+    month_row
+
+    #if within the quarter there's a month that has 8 rows, add a blank row
+    #to the end of the other two months in the quarter
+    #(make all months) have an equal number of rows
+    0.upto(3) do |quarter_index|
+      0.upto(2) do |month_index|
+        if month_row[quarter_index][month_index].length == 8
+          long_qtrs = month_row[quarter_index]
+          long_qtrs.each do |mth|
+            blank_row = "                    "
+            if mth.length == 7
+              mth << blank_row
+            end
+          end
+        end
+      end
     end
     month_row
   end
