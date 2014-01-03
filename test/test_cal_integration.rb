a3f6c6453b001d6e8f28242856abbf3535eb40ce
Lori McCurry <lori.mccurry@gmail.com>
2014-01-03 10:07:24 -0600

    clean up cal script testing

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -1,18 +1,9 @@
 require 'minitest/autorun'
-# require_relative '../cal.rb'
 require_relative 'helper'
 
 class TestCalIntegration < MiniTest::Unit::TestCase
 
-#test here for cal.rb stuff; test argv values
-#also if year is between 1800 and 3000
-
-  def test_01_ARGV_inputs
-    shell_output = `ruby cal.rb 2 2012`
-    assert_equal(["2", "2012"], ARGV)
-  end
-
-  def test_02_month_output_1_2012
+  def test_01_month_output_1_2012
     shell_output = `ruby cal.rb 1 2012`
     expected_output = <<EOS
     January 2012
@@ -26,7 +17,7 @@ EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_03_month_output_2_2012
+  def test_02_month_output_2_2012
     shell_output = `ruby cal.rb 02 2012`
     expected_output = <<EOS
    February 2012
@@ -35,12 +26,12 @@ Su Mo Tu We Th Fr Sa
  5  6  7  8  9 10 11
 12 13 14 15 16 17 18
 19 20 21 22 23 24 25
-26 27 28 29
+26 27 28 29#{"         "}
 EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_04_month_output_9_2012
+  def test_03_month_output_9_2012
     shell_output = `ruby cal.rb 9 2012`
     expected_output = <<EOS
    September 2012
@@ -56,7 +47,7 @@ EOS
   end
 
 
-  def test_05_month_output_2_2015
+  def test_04_month_output_2_2015
     shell_output = `ruby cal.rb 02 2015`
     expected_output = <<EOS
    February 2015
@@ -69,7 +60,7 @@ EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_06_month_output_2_1900
+  def test_05_month_output_2_1900
     shell_output = `ruby cal.rb 2 1900`
     expected_output = <<EOS
    February 1900
@@ -83,7 +74,7 @@ EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_07_month_output_2_2000
+  def test_06_month_output_2_2000
     shell_output = `ruby cal.rb 2 2000`
     expected_output = <<EOS
    February 2000
@@ -97,7 +88,7 @@ EOS
     assert_equal expected_output, shell_output
   end
 
-  def test_08_year_output_2012
+  def test_07_year_output_2012
     shell_output = `ruby cal.rb 2012`
     expected_output = <<EOS
                             2012
@@ -109,7 +100,6 @@ Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
 22 23 24 25 26 27 28  19 20 21 22 23 24 25  18 19 20 21 22 23 24
 29 30 31              26 27 28 29           25 26 27 28 29 30 31
 
-
        April                  May                   June
 Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
  1  2  3  4  5  6  7         1  2  3  4  5                  1  2
@@ -117,7 +107,6 @@ Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
 15 16 17 18 19 20 21  13 14 15 16 17 18 19  10 11 12 13 14 15 16
 22 23 24 25 26 27 28  20 21 22 23 24 25 26  17 18 19 20 21 22 23
 29 30                 27 28 29 30 31        24 25 26 27 28 29 30
-
 
         July                 August              September
 Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
@@ -127,7 +116,6 @@ Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
 22 23 24 25 26 27 28  19 20 21 22 23 24 25  16 17 18 19 20 21 22
 29 30 31              26 27 28 29 30 31     23 24 25 26 27 28 29
                                             30
-
       October               November              December
 Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
     1  2  3  4  5  6               1  2  3                     1
